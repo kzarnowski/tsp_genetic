f17291762bb12d1b6925d3d3c27f952ceb2d1228
kzarnowski <zarnowski@student.agh.edu.pl>
2022-06-30 17:25:52 +0200

    algo finished

--- configuration.py
+++ configuration.py
@@ -1,17 +1,17 @@
-RANDOM_MODE = True
+RANDOM_MODE = False
 
-# only if RANDOM_MODE == FALSE
+# only if RANDOM_MODE == True
 WIDTH = 100
 HEIGHT = 150
-NUM_OF_CITIES = 6 # MAX: 65535
+NUM_OF_CITIES = 15 # MAX: 65535
 # ----------------------------
 
 CITIES_FILEPATH = r'data/p1.txt'
 
-POPULATION_SIZE = 10
-MUTATION_PROBABILITY = 0.3
-MAX_ITER = 10  # liczba iteracji bez poprawy rozwiązania po których algorytm się zatrzyma
+POPULATION_SIZE = 10000
+MUTATION_PROBABILITY = 0.1
+MAX_ITER = 1000  # liczba iteracji bez poprawy rozwiązania po których algorytm się zatrzyma
 
-PARENTS_RATIO = 0.2
+PARENTS_RATIO = 0.5
 INCLUDE_PARENTS = True
 
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.spatial import distance_matrix
+from random import random
+
 
 import configuration as CONFIG
 
@@ -57,21 +59,36 @@ def select_parents(pop, scores):
     return parents
 
 
+def random_pair(max_value):
+    rng = np.random.default_rng()
+    idx = rng.choice(max_value, 2, replace=False)
+    return idx[0], idx[1]
+
+
+def generate_pairs(parents):
+    if CONFIG.INCLUDE_PARENTS:
+        offspring_size = CONFIG.POPULATION_SIZE - parents.shape[0]
+    else:
+        offspring_size = CONFIG.POPULATION_SIZE
+
+    pairs = np.ndarray(shape=(offspring_size//2, 2), dtype=np.uint16)
+
+    # TODO: remove loop
+    for i in range(pairs.shape[0]):
+        pairs[i, :] = random_pair(parents.shape[0])
+
+    return pairs
+
+
 def crossover(p1, p2):
     end = CONFIG.NUM_OF_CITIES - 1
-    rng = np.random.default_rng()
-    indices = rng.choice(end, 2, replace=False)
-    u, v = indices[0], indices[1]
-
-    print(f"u: {u}  v: {v}\n")
+    u, v = random_pair(end)
 
     if v < u:
         u, v = v, u
     if v - u < 2:
         v = min(v + 1, end)
         u = max(u - 1, 0)
-
-    print(f"u: {u}  v: {v}\n")
 
     o1 = np.zeros(CONFIG.NUM_OF_CITIES, dtype=np.uint16)
     o2 = np.zeros(CONFIG.NUM_OF_CITIES, dtype=np.uint16)
@@ -82,13 +99,8 @@ def crossover(p1, p2):
     p1_rest = np.roll(p1, -(v+1))
     p2_rest = np.roll(p2, -(v+1))
 
-    print(f"p1_rest: {p1_rest}\np2_rest: {p2_rest}")
-
     o1_fill = p2_rest[~np.in1d(p2_rest, o1[u:v+1])]
     o2_fill = p1_rest[~np.in1d(p1_rest, o2[u:v+1])]
-
-
-    print(f"o1_fill: {o1_fill}\no2_fill: {o2_fill}")
 
     if v != end:
         o1[v+1:end+1] = o1_fill[:end-v]
@@ -101,6 +113,60 @@ def crossover(p1, p2):
     return o1, o2
 
 
+def mutate(pop):
+    def mutate_individual(x):
+        u, v = random_pair(x.shape[0])
+        x[u], x[v] = x[v], x[u]
+        return x
+
+    def mutate_offsprings(offsprings):
+        return np.apply_along_axis(mutate_individual, 1, offsprings)
+
+    is_mutated = np.array(np.random.rand(pop.shape[0]) <= CONFIG.MUTATION_PROBABILITY)
+    pop[is_mutated] = mutate_offsprings(pop[is_mutated])
+    return pop
+
+
+def procreate(parents, pairs):
+    offspring = np.ndarray(shape=(
+        2*pairs.shape[0],
+        parents.shape[1]
+    ), dtype=np.uint16)
+
+    i = 0
+    while i < 2*pairs.shape[0]:
+        p1 = parents[pairs[i//2, 0], :]
+        p2 = parents[pairs[i//2, 1], :]
+        o1, o2 = crossover(p1, p2)
+        offspring[i, :] = o1
+        offspring[i+1, :] = o2
+        i += 2
+    return offspring
+
+
+def next_generation(parents, offspring):
+    if CONFIG.INCLUDE_PARENTS:
+        return np.vstack((parents, offspring))
+    else:
+        return offspring
+
+
+def stop_condition(scores):
+    global iters_without_change
+    global best_idx
+    global best_score
+
+    curr_best_idx = np.argmin(scores)
+    curr_best_score = scores[curr_best_idx]
+
+    if curr_best_score < best_score:
+        best_score = curr_best_score
+        best_idx = curr_best_idx
+        iters_without_change = 0
+        return False
+    else:
+        iters_without_change += 1
+        return iters_without_change == CONFIG.MAX_ITER
 
 
 if __name__ == '__main__':
@@ -111,13 +177,25 @@ if __name__ == '__main__':
     population = init_population()
     scores = eval_population(population)
 
-    parents = select_parents(population, scores)
+    iters_without_change = 0
+    best_idx = np.argmin(scores)
+    best_score = scores[best_idx]
 
-    offspring = crossover(population[0], population[1])
+    gen = 0
+    while not stop_condition(scores):
+        if gen % 10 == 0:
+            print(f"GEN: {gen} BEST: {best_score} SOLUTION: {population[best_idx]} ALL: {np.sum(scores)}")
+        gen += 1
 
-    print("PARENTS:\n", population[0], population[1])
-    print("\nOFFSPRING\n", offspring[0], offspring[1])
+        parents = select_parents(population, scores)
+        pairs = generate_pairs(parents)
+        offspring = procreate(parents, pairs)
 
-    # for i in range(N-1):
-    #     plt.plot(cities[i:i+2, 0], cities[i:i+2, 1], 'ro-')
-    # plt.show()
+        offspring = mutate(offspring)
+
+        population = next_generation(parents, offspring)
+        scores = eval_population(population)
+
+
+
+
